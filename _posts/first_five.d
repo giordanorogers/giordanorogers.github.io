e5a17962cb81287ea1e912ca53ba6d3b8416befe
Giordano Rogers <giordanopaulrogers@gmail.com>
2026-03-10 17:21:48 -0400

    added agents of chaos

---
+++ _posts/first_five.d
@@ -0,0 +1,138 @@
+---
+title: 'First Five'
+date: 2999-12-31
+permalink: /fictions/first_five/
+---
+
+## 1
+
+The bed. The child jumping. Me. The glee. The air. It's fun. 
+
+My parents' room taken up by a king bed. Maroon covers. A darkwood frame with masts that go to the ceiling. I'm four. I'm looking down and around me at the covers and the tables and the rug and the kitchen through the door and I'm jumping.
+
+In the kitchen, mom and dad are making food. Washing dishes. Talking. Moving about. The light is on and the fan and a TV's playing shows. It's night. I was bored with nothing to do now I'm jumping on the bed having fun.
+
+My head is shaved. A week earlier, it's curls and locks. My mother has me out in the front yard shirtless. She's sat on cement by the flower bed, taking a buzzer to me. Looking out at the street. Triple-families and parked cars and a long road.
+
+Loose tufts brush down my brow and cheeks. I turn and see my father up the stairs.
+
+"Whuh! Look at that! The mop is gone. You must feel amazing huh? Five pounds lighter."
+
+Now my head's been trimmed a week but I still like to rub it. Grazing my palm on the prickles.
+
+I'm on the bed and I'm jumping, jumping. Looking at the mail and jewelry and medicine strewn about on my mother's bedside table. The shoes strewn about. The clothes on the hangers past the curtains of the closet. I'm jumping. Jumping. Slipping! Falling! Looking down at the rug.
+
+
+Darkness. Time. A car ride. Watching the blurry trees. Hearing my father's voice.
+
+"He wouldn't even have to get stitches if she hadn't shaved his head. They could have just tied his hair up tight. Now he's gonna have a scar."
+
+## 2
+
+Scribbling green brown orange crayons. The zombie extending his hands. Grasping out off the page. A pack eat a girl by a bush in the background. Set against purple horizon.
+
+I got comics from the bookfair. My favorite to read. Now I'm making one. Folding and stapling paper into a booklet. Drawing my zombies at the table in the kitchen. Grandma humming and cooking.
+
+I'm done. No story. Nothing more than a cover and part of a sketch on the first inside page. I hand it to grandma. Scrunches her brow as she's looking to see it. Then she's oohing and aahing and makes her reaction a show.
+
+"Ah! Dano, you draw dis!? It look nice!"
+
+Big grin on my face. Gripping a crayon. Looking up at my grandma as she looks at the page then at me and then back at the comic I made. Her face wrinkled up and her mouth is agape.
+
+"I made it for you."
+
+The old lady smiles and kneels down and hugs me.
+
+"Thank you baby."
+
+She goes to the fridge, covered with papers and letters and pictures of uncles and aunties and cousins and strangers. Finding a magnet, she ask for permission to hang what I made on the door so she'll see it whenever she wants. I smile and nod.
+
+Making space in the center, she magnets my work to the fridge, just a little higher than my head.
+
+We stand and look at it for more than a moment.
+
+## 3
+
+The first day of school. We ride up and park by the gate at the front of the building. Through the tinted window I see kids with their parents. Walking up the street. Holding mommy's hand. All of us in cherry red cardigans.
+
+Nervous. I wait for mom to get out out of the car. She walks around, opens my door and unbuckles me.
+
+"Come on now Dano."
+
+I reach for her hand and she takes mine in hers and I chew on the sleeve of my free arm.
+
+The kids are all filing in through the door. Two ladies standing there smiling, waving. One a bit taller. Blonde, looking way off down the road. The other one plumper, dressed in a blazer and greeting each family that comes up the pebblestone steps.
+
+I'm still on the sidewalk outside the car door. Mom's looking down. Watching my expression. Me chewing on sleeve and just standing and staring.
+
+"Come on Dano. It's gonna be ok. You're gonna have fun."
+
+Mom takes the lead and brings me up to the mouth of the gate. Then she lets go.
+
+"Go ahead Dano. I'll be here to pick you up when school is over."
+
+A look. A nod. Hesitation in the form of timid steps. But I go ahead.
+
+The lady in the blazer is looking down at me. I'm walking up steps and meeting her gaze. She opens her arms.
+
+"Well hel-lo!" she chimes.
+
+Startled! I unclench my jaw, releasing my spittled up sleeve from my teeth and then thwhack my fist into her breast. Turning for my mom. Running down the steps. Straight into her legs with my hands around her thighs. Hoping I can stay.
+
+## 4
+
+White speckled dots twinkling all 'round my head through the blackness. A trio of fighter ships soar in the distance. My view pans down slowly to rocky terrain, under the infinite starry-eyed space.
+
+A spherical ship touches down to my right. Opens its hatch. From inside, a tiny spaceman appears. Human, I think. Red-white commando climbing down the ladder. Right up to me. Words flash on the screen.
+
+"WHAT IS YOUR NAME, STRANGER?"
+
+A cursor blinks. But I don't have a controller to type with. Confused, I grab at the sides of my helmet and pull it off my head.
+
+"How was it?" mom asks.
+
+Brightness floods my eyes. I'm light headed. Everything's gouache before coming in focus. I'm back in the kitchen. Surrounded by family.
+
+I don't answer. I look at the helmet. I look for a remote maybe latched in its sides. No luck. I pick up the box and rummage around till I'm sure there's no way there's a controller I'm missing.
+
+"I need a controller. It's asking for my name but I don't know how to type it."
+
+The adults help me look while my brother puts the helmet on and tries to see if he can figure out the issue with the game. He taps around the helmet-top and back and sides for buttons. Then he takes it off.
+
+"Yeah. I don't know."
+
+"We'll figure it out in a minute Dano but for now open another gift."
+
+I twich out a shrug of exasperation. I already got my scooter. All the other gifts are tiny. I want to play the game. I throw a fit.
+
+Later on, my brother reads the box.
+
+"It's definitely supposed to come with controllers. But I don't see them anywhere."
+
+I don't get to play my game. Days pass. Then I can't find my helmet anywhere. I ask my mom about it. She tells me not to worry.
+
+## 5
+
+The apartment has a long hallway leading from the front door to the kitchen. The ceiling feels so high. One wall's covered in pictures. Paintings. Sketches from my uncle. Two naked ladies. I sometimes stop and stare.
+
+The lightswitch is at the front. Right now the light is off. The hall is dark.
+
+My back is to the kitchen. But I can't see the other end. Something's in my way. Legs. Tall. Everything is quiet. We're alone.
+
+In front of me. Heat. In front of me. Silence. Heartbeat.
+
+A sour taste.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
